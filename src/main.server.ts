a4d7cb6cffb43cb218e4d88035253df8a392fe71
MaiChinu <chisanupolwo@gmail.com>
2024-02-28 09:13:10 +0700

    Edit main.sever file and add test.txt

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstraped = () => bootstrapApplication(AppComponent, config);
 
-export default bootstrap;
+export default bootstraped;
+
+// This is test is not working
